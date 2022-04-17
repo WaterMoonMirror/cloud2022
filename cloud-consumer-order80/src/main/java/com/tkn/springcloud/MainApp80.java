3ef1d618f782d0e83765b9bc42c7fc2ca7f3dbd5
李柱 <lizhu@wondersgroup.com>
2022-04-17 11:35:17 +0800

    cloud-consumer-order80 微服务消费者订单Module模块

---
+++ cloud-consumer-order80/src/main/java/com/tkn/springcloud/MainApp80.java
@@ -0,0 +1,14 @@
+package com.tkn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp80
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MainApp80.class,args);
+    }
+}
+
