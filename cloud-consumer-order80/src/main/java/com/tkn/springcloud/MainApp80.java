da6078583f3f0d1d3e924c382183b01162f4ab22
李柱 <lizhu@wondersgroup.com>
2022-04-17 21:35:21 +0800

    EurekaClient端cloud-consumer-order80 将注册进EurekaServer成为服务消费者consumer

--- cloud-consumer-order80/src/main/java/com/tkn/springcloud/MainApp80.java
+++ cloud-consumer-order80/src/main/java/com/tkn/springcloud/MainApp80.java
@@ -2,8 +2,10 @@ package com.tkn.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MainApp80
 {
     public static void main(String[] args)
