be882b51617512ae845c28150199549ec8d98e7e
李柱 <lizhu@wondersgroup.com>
2022-05-12 10:44:06 +0800

    OpenFeign使用步骤

---
+++ cloud-consumer-feign-order80/src/main/java/com/tkn/springcloud/controller/OrderFeignController.java
@@ -0,0 +1,23 @@
+package com.tkn.springcloud.controller;
+
+import com.tkn.springcloud.entities.CommonResult;
+import com.tkn.springcloud.entities.Payment;
+import com.tkn.springcloud.service.PaymentFeignService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class OrderFeignController
+{
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id)
+    {
+        return paymentFeignService.getPaymentById(id);
+    }
+}
